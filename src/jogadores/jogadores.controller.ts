dd8ea28af83e0b46dc1389bf5c365fbadab3bfae
Mateus Souza <mateus_souza11@hotmail.com.br>
2022-02-22 11:30:47 -0300

    Criando jogadores service

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 
-@Controller('jogadapi/v1/jogadores')
+@Controller('api/v1/jogadores')
 export class JogadoresController {
   @Post()
-  async criarAtualizarJogador() {
-    return JSON.stringify({
-      nome: 'Diego',
-    });
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    const { email } = criarJogadorDto;
+    return JSON.stringify(`{
+        "email": ${email} 
+      }`);
   }
 }
