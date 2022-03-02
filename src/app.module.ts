bf6b31d17e4f38f9e74631416b5be768c576004b
Mateus Souza <mateus_souza11@hotmail.com.br>
2022-03-02 15:30:52 -0300

    Criando jogador.schema.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://mateusilva11:74123@cluster0.ifmir.mongodb.net/smartranking?retryWrites=true&w=majority',
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
